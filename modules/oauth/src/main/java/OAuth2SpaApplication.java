d191eff02b6395099710548aee98a6990f0f7497
okowu <devs.okowu@gmail.com>
2025-07-12 15:06:04 +0200

    apply spotless

--- modules/oauth/src/main/java/OAuth2SpaApplication.java
+++ modules/oauth/src/main/java/OAuth2SpaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OAuth2SpaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OAuth2SpaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OAuth2SpaApplication.class, args);
+  }
 }
